15e37fa7c9cfd37dd27cf29e05d38e75db0bc0eb
TanwaratSalee <tanwas.17@gmail.com>
2024-05-20 01:48:11 +0700

    TermAndConditions buttom

---
+++ lib/Views/auth_screen/ termAndConditions.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class  TermAndConditions extends StatelessWidget {
+  const  TermAndConditions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
