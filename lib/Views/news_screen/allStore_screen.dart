7409ccfa0100b81cf8a74f93fb687faa571d6aac
Captainaint <natthawut.aint@bumail.net>
2024-05-31 01:14:11 +0700

    allstore

--- lib/Views/news_screen/allStore_screen.dart
+++ lib/Views/news_screen/allStore_screen.dart
@@ -1,11 +1,139 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_finalproject/Views/cart_screen/cart_screen.dart';
+import 'package:flutter_finalproject/consts/colors.dart';
+import 'package:flutter_finalproject/consts/images.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class AllStoreScreen extends StatelessWidget {
-  const AllStoreScreen({super.key});
+  const AllStoreScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    List<String> officialBrandNames = [
+      'DIOR',
+      'BALENCIAGA',
+      'VERSACE',
+      'LV',
+      'CHANEL',
+      'H'
+    ];
+    List<String> newBrandNames = [
+      'GUCCI',
+      'PRADA',
+      'FENDI',
+      'BURBERRY',
+      'SAINT LAURENT',
+      'VALENTINO'
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: whiteColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(height: 20),
+            Image.asset(icLogoOnTop, height: 40),
+            IconButton(
+              icon: Image.asset(icCart, width: 21),
+              onPressed: () {
+                Get.to(() => const CartScreen());
+              },
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'OFFICIAL STORE',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6
+                      ?.copyWith(color: Colors.black),
+                ),
+              ),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.1,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemCount: officialBrandNames.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Text(officialBrandNames[index]),
+                      ),
+                    ],
+                  );
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'NEW STORE',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6
+                      ?.copyWith(color: Colors.black),
+                ),
+              ),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.1,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemCount: newBrandNames.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Text(newBrandNames[index]),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
