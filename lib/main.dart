093b109c86193bb0c1cfe835d5452f344dd72fc6
TanwaratSalee <tanwas.17@gmail.com>
2024-06-25 19:08:42 +0700

    oo

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:flutter_finalproject/consts/styles.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
