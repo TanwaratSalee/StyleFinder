493574b5309b57e643ce79328eaacf0ad61321c9
Captain <Natthawut.aint@bumail.net>
2024-07-06 17:04:11 +0700

    newpageSituationMatching

--- lib/consts/colors.dart
+++ lib/consts/colors.dart
@@ -16,5 +16,7 @@ const Color greyDark = Color.fromRGBO(77, 76, 76, 1);
 const Color whiteOpacity = Color.fromRGBO(237, 235, 235, 0.918);
 
 const Color greyMessage = Color.fromRGBO(225, 225, 225, 1);
-const Color primaryMessage = Color.fromRGBO(94, 213, 212 ,1);
+const Color primaryMessage = Color.fromRGBO(94, 213, 212, 1);
 // const Color primaryMessage = Color.fromRGBO(121, 211, 211, 1);
+
+const Color primaryfigma = Color.fromRGBO(199, 249, 249, 1);
